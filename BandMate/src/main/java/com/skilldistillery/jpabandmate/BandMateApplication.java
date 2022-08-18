9dc32d0cd8d0a389bdd5b271e513f4c592d034e2
Thao Nguyen <thaontt1408@gmail.com>
2022-08-18 11:58:31 -0600

    Create Boot MVC project

---
+++ BandMate/src/main/java/com/skilldistillery/jpabandmate/BandMateApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpabandmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandMateApplication.class, args);
+	}
+
+}
---
+++ BandMate/src/test/java/com/skilldistillery/jpabandmate/BandMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpabandmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
